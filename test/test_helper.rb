cc92563f64f58a9457641d6c2e6aedf8db450de7
sudhanshusr <sudhanshusr@cybage.com>
2016-03-07 12:33:19 +0530

    Update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
+require 'simplecov-rcov'
 require 'minitest/hyper'
 require 'simplecov'
 SimpleCov.start
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
